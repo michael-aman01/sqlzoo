b5295710763d7c06d6535f4f348d516088fee3b9
michael <michael_aman92@icloud.com>
2022-09-06 20:14:10 -0700

    add 03_select

--- lib/02_select_countries.rb
+++ lib/02_select_countries.rb
@@ -25,32 +25,69 @@ def large_countries
   # Show the names of the countries that have a population of at least
   # 200 million. 200 million is 200,000,000 (eight zeros).
   execute(<<-SQL)
+  SELECT
+  name
+  FROM 
+  countries
+  WHERE
+  population >= 200000000
   SQL
 end
+
+p large_countries
 
 def high_population_gdps
   # Give the names and the per capita GDPs of countries with a population
   # of at least 200 million.
   execute(<<-SQL)
+  SELECT
+  name, gdp/population
+  FROM
+  countries
+  WHERE
+  population > 200000000
   SQL
 end
+p high_population_gdps
 
 def population_in_millions
   # Show the name and population in millions for the countries with continent
   # 'South America'. Divide the population by 1,000,000 to get population in
   # millions.
   execute(<<-SQL)
+  SELECT
+  name, population/1000000
+  FROM 
+  countries
+  WHERE
+  continent = 'South America'
   SQL
 end
+
+p population_in_millions
 
 def name_and_population
   # Show the name and population for 'France', 'Germany', and 'Italy'
   execute(<<-SQL)
+  SELECT
+  name, population
+  FROM
+  countries
+  WHERE
+  name IN ('France','Germany','Italy')
   SQL
 end
-
+p name_and_population
 def united_we_stand
   # Show the countries that have a name that includes the word 'United'
   execute(<<-SQL)
+  SELECT 
+  name
+  FROM 
+  countries
+  WHERE
+  name LIKE 'United%'
   SQL
 end
+
+p united_we_stand
